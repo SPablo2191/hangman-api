f5ff7fdf8d6db5202fd7c8e6a202df69833e34f8
SPablo2191 <pablosandoval2191@gmail.com>
2023-10-06 13:58:48 -0300

    fix: snake case correction

--- project/app/main.py
+++ project/app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core import config
 from app.core.endpoints import Endpoints
-from app.routers.games import router as gamesRouter
+from app.routers.games import router as games_router
 
 app = FastAPI()
 app.include_router(
-    gamesRouter, tags=[Endpoints.games], prefix=config.api_url + Endpoints.games
+    games_router, tags=[Endpoints.games], prefix=config.api_url + Endpoints.games
 )
 
 app.add_middleware(
