c4d74cf0b0ed0c3178b669048d524bb5667da710
SPablo2191 <pablosandoval2191@gmail.com>
2023-10-06 13:58:34 -0300

    feat: add gamesrouter and cors

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core import config
+from app.core.endpoints import Endpoints
+from app.routers.games import router as gamesRouter
+
 app = FastAPI()
+app.include_router(
+    gamesRouter, tags=[Endpoints.games], prefix=config.api_url + Endpoints.games
+)
 
-
-@app.get("/")
-async def root():
-    return {"message": config.api_url}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
